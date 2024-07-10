f6d13a29f4a962b0e6ebab6df274406472ac6a32
Aas Suhendar <aas.suhendar@gmail.com>
2024-07-10 22:35:57 +0700

    Update installer-packages-devops-ubuntu.sh

--- installer-packages-devops-ubuntu.sh
+++ installer-packages-devops-ubuntu.sh
@@ -66,30 +66,35 @@ installDocker(){
     printHeader
 	echo "==> Installing Docker"
 	echo "=================================================="
-    apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
+
+  	apt-get install ca-certificates curl
+	install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt-get update
+
+    	apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     
-    systemctl start docker
-    systemctl enable docker
-    echo -e "You want add user to group docker ? [y/n]: \c"
-    read option
-    if [ $option = Y -o $option = y ]
-    then
-        echo -e "Username for add to group docker : \c"
-        read useradd
-        usermod -aG docker $useradd
-    fi
-    docker info
-    echo "=================================================="
-    echo "Done, Completed!"
+	    echo -e "You want add user to group docker ? [y/n]: \c"
+	    read option
+	    if [ $option = Y -o $option = y ]
+	    then
+	        echo -e "Username for add to group docker : \c"
+	        read useradd
+	        usermod -aG docker $useradd
+	    fi
+     
+	docker info
+	docker --version
+	docker compose version
+	echo "=================================================="
+	echo "Done, Completed!"
 }
 
 
